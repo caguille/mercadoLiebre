25d34bb3dc8aeedd18451f6d0ca4e9602b2f24d8
caguille <cristianma26@gmail.com>
2022-04-20 01:23:48 -0300

    sube mercadoLiebra

---
+++ app.js
@@ -0,0 +1,11 @@
+const express= require ('express');
+const app= express();
+const path=require ('path');
+const publicPath= path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.get('/', (req,res)=> {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
+
+app.listen(5000,()=> console.log ('El servidor 5000 corre sin problema'));
